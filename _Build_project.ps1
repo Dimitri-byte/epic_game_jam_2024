b7d3fe9360ebe0732a830e8b7a15e31aa89f3b86
Dimitri Biard <dbiard@gmail.com>
2024-06-25 19:51:33 +0200

    added basic main menu

---
+++ _Build_project.ps1
@@ -0,0 +1,30 @@
+# Définir les variables
+$projectPath = Join-Path -Path (Get-Location) -ChildPath "epic_game_jam_2024.uproject"
+$outputpath = Join-Path -Path (Get-Location) -ChildPath "Build"
+$dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+$outputDir = $outputpath + $dateTime
+$buildConfig = "Shipping"
+$platform = "Win64"
+
+# Construire le projet
+& "C:\Program Files\Epic Games\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun `
+    -project="$projectPath" `
+    -noP4 `
+    -platform="$platform" `
+    -clientconfig="$buildConfig" `
+    -serverconfig="$buildConfig" `
+    -cook `
+    -allmaps `
+    -build `
+    -stage `
+    -pak `
+    -archive `
+    -archivedirectory="$outputDir"
+
+# Vérifier si le dossier de build existe
+if (-Not (Test-Path -Path "$outputDir")) {
+    Write-Error "Le chemin de build `$outputDir` est introuvable."
+    exit 1
+}
+
+Write-Output "Deployment completed successfully!"
